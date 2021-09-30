6e0efe3ef9926d96d26507a2b7fd8ca1d6493afa
kyllian18 <64402572+kyllian18@users.noreply.github.com>
2021-09-30 11:25:49 +0100

    my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace csharp_git_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is version 1");
+        }
+    }
+}
